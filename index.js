d963ff3c76dda71360e8996493ffdb1764ea0b1d
Michael Suzio <msuzio@gmail.com>
2017-10-17 19:03:24 -0400

    Hello World (literally) application up

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict'
+const express = require('express');
+const app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+    res.status(200).send('Hello World');
+});
+
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
