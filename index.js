91ec512dc3660477f01a8574162cf93eab12e09d
Michael Suzio <msuzio@gmail.com>
2017-10-17 20:21:41 -0400

    stubbed out some GET handlers

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 'use strict'
 const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+//
+// We will want to handle both "simple" POSTs and REST api calls for testing
+//
+// parse application/x-www-form-urlencoded 
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// parse application/json 
+var jsonParser = bodyParser.json();
+
+
 app.get('/', function (req, res) {
-    res.status(200).send('Hello World');
+    res.redirect('/register');
 });
 
-  app.listen(app.get('port'), function() {
+app.get('/register',  function(req,res) {
+    res.status(200).send("On registration page");
+});
+
+app.get('/report', function(req,res) {
+    res.status(200).send("On report Page");
+});
+
+app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
-  });
+});
